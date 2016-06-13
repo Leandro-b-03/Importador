e7bfb349030f35a278a3484ec3667388b0d29cc3
leandro-b-03 <leandro.b.03@gmail.com>
2016-06-13 11:14:07 -0300

    Menu Fix

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,19 +28,16 @@
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-                    <a class="navbar-brand" href="#">Start Bootstrap</a>
+                    <a class="navbar-brand" href="/">Importador SVLabs</a>
                 </div>
                 <!-- Collect the nav links, forms, and other content for toggling -->
                 <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                     <ul class="nav navbar-nav">
                         <li>
-                            <a href="#">About</a>
+                            <a href="columns">Colunas não cadastradas</a>
                         </li>
                         <li>
-                            <a href="#">Services</a>
-                        </li>
-                        <li>
-                            <a href="#">Contact</a>
+                            <a href="log-viewer" target="_blank">Ver Logs</a>
                         </li>
                     </ul>
                 </div>
